5896bc39f52cc5d23515c9aa39c8ff50ee16a8cd
AntoninMignotPilon <antonin.mignotpilon@viacesi.fr>
2026-01-18 14:36:44 +0100

    Ajout de l'endpoint register et de l'interface

--- src/main/java/com/codestar/backend/controller/AuthController.java
+++ src/main/java/com/codestar/backend/controller/AuthController.java
@@ -2,6 +2,8 @@ package com.codestar.backend.controller;
 
 import com.codestar.backend.dto.LoginRequestDto;
 import com.codestar.backend.dto.LoginResponseDto;
+import com.codestar.backend.model.User;
+import com.codestar.backend.repository.IUserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +12,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/auth")
 @CrossOrigin(origins = "*")
 public class AuthController {
+
+    private final IUserRepository userRepository;
+
+    public AuthController(IUserRepository userRepository){
+        this.userRepository = userRepository;
+    }
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto request){
@@ -20,6 +28,11 @@ public class AuthController {
         else{
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
+    }
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user){
+        return userRepository.save(user);
     }
 
 }
