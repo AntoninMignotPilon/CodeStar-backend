85ccda2eaeea76d3fea40abac78c91323d7a723b
Timothé <timothe.fardella@viacesi.fr>
2026-03-03 20:24:48 +0100

    chore : update app properties + add README

---
+++ README.md
@@ -0,0 +1,2 @@
+Command to execute the back : `.\mvnw.cmd spring-boot:run` 
+Make sure you up your bdd on docker (docker compose up -d)
--- src/main/java/com/codestar/backend/config/SecurityConfig.java
+++ src/main/java/com/codestar/backend/config/SecurityConfig.java
@@ -8,20 +8,20 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-    @Configuration
-    @EnableWebSecurity
-    public class SecurityConfig {
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
 
-        @Bean
-        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-            http.csrf(csrf -> csrf.disable())
-                    .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
-            return http.build();
-        }
-
-        @Bean
-        public PasswordEncoder passwordEncoder() {
-            return new BCryptPasswordEncoder();
-        }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
 
